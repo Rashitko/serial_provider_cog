d44d44f20446a1cab7dd16352d50911a1147ae23
Michal Raška <michal@Michals-MacBook-Air.local>
2017-04-19 10:59:29 +0200

    bugfixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='serial_cog',
+    name='serial_provider_cog',
     version='0.1.4',
     packages=['serial_cog', 'serial_cog.modules'],
     url='https://github.com/Rashitko/serial_provider_cog',
